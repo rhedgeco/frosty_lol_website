6b52bd28fdb482bdbf211178fa95660a922d5021
rhedgeco <rhedgeco@gmail.com>
2020-09-04 16:00:13 -0700

    added submodules, .gitignore, and setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='frosty_lol_website',
+    version='1.0.0',
+    install_requires=[
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "cachecontrol",
+        'google',
+        'requests',
+        'falcon',
+        'httpie',
+        'pytest'
+    ]
+)
