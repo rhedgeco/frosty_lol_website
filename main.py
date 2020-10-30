fbf86929de0b58ba2ae1ff7fa76f0729172399c7
rhedgeco <rhedgeco@gmail.com>
2020-10-29 21:53:50 -0700

    use cors in sanic webserver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from sanic import Sanic, response
-from sanic.websocket import WebSocketProtocol
+from sanic_cors import CORS
 
 from backend.chat_websocket import ChatManager
 from backend.database_handlers.databases import SqliteDatabase
@@ -8,6 +8,7 @@ from backend.database_manager import DatabaseManager
 from backend.google_oauth import GoogleOauth
 
 app = Sanic('frosty_lol')
+CORS(app)
 
 frontend_dir = Path('./frontend')
 app.static('/', str(frontend_dir))
